8d057586463a44511c0dc22c23efc8cb96f7699a
Simon Schuhmacher <91273944+simonschuhmacher@users.noreply.github.com>
2023-05-24 11:33:07 +0200

    add new iOS project

---
+++ TronaldDump/TronaldDumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TronaldDumpApp.swift
+//  TronaldDump
+//
+//  Created by Simon Schuhmacher on 24.05.23.
+//
+
+import SwiftUI
+
+@main
+struct TronaldDumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
